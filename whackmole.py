e9eb42bb58e882e794d39cf6175c5d36a0470dce
km71031 <suzukisan210@gmail.com>
2016-07-16 14:41:28 +0900

    Add files via upload

--- whackmole.py
+++ whackmole.py
@@ -23,13 +23,16 @@ rrr = 12
 #enemymove = False
 
 class Mole(Widget):
-    score = NumericProperty(0)
-    time = NumericProperty(20)
+
     #Color = (1, 0, 0)
     #def boxattack(self, en):
      #  if self.collide_widget(en):
 
-    def whack(self, mole):
+    def whack(self, hm):
+        """if self.collide_widget(hm):
+            self.mole.x = 900 * random.random()
+            self.mole.y = 500 * random.random()
+            self.score += 1"""
         pass
 
     """
@@ -55,16 +58,18 @@ class Hammer(Mole):
 func(a=1,b=2)"""
 
 class MoleGame(Widget):
-    mole = ObjectProperty(None)
-    hammer = ObjectProperty(None)
-    mole = Mole()
-    hammer = Hammer()
+    mole = ObjectProperty(Mole)
+    hammer = ObjectProperty(Hammer)
+    score = NumericProperty(0)
+    time = NumericProperty(1000)
+    #mole = Mole()
+    #hammer = Hammer()
     def start(self):
         #self.mole = Mole()
         #self.hammer =Hammer()
-        wpx = int(math.ceil(500 * random.random()))
-        wpy = int(math.ceil(500 * random.random()))
-        self.mole.center = [int(100), int(300)]
+        #wpx = int(math.ceil(500 * random.random()))
+        #wpy = int(math.ceil(500 * random.random()))
+        self.mole.center = [100, 300]
         self.hammer.center= [450, 250]
         print self.mole.center
         print self.hammer.center
@@ -82,26 +87,40 @@ class MoleGame(Widget):
      #   self.player1.center = touch
 
     def on_touch_move(self, touch):
-        moleX = self.mole.x
-        if self.mole.time:
+        #moleX = self.mole.x
+        hammerX = self.hammer.x
+        hammerY = self.hammer.y
+        if self.time:
+            """
             if moleX < touch.x < moleX + self.mole.width:
                 self.mole.x = 900 * random.random()
                 self.mole.y = 500 * random.random()
-                self.mole.score += 1
-        #else:
-           # self.mole.time =
+                self.score += 1
+            #"""
+            if hammerX < touch.x < hammerX + self.hammer.width:
+                if hammerY< touch.y < hammerY + self.hammer.width:
+                    self.hammer.x = touch.x - self.hammer.width / 2
+                    self.hammer.y = touch.y - self.hammer.height / 2
 
-
+    #hammer = Hammer()
     def update(self, dt):
        # for en in self.enemy:
         #    self.p1.pcrush(en)
         #self.mole.on_touch_move
         #self.mole.whack(self)
 
-        if self.mole.time:
-            self.mole.time -= 1
+        if self.mole.collide_widget(self.hammer):
+           self.mole.x = 900 * random.random()
+           self.mole.y = 500 * random.random()
+           self.hammer.x = 900 * random.random()
+           self.hammer.y = 500 * random.random()
+           self.score += 1
+
+
+        if self.time:
+            self.time -= 1
         else:
-            self.mole.time = 0
+            self.time = 0
 
         """if self.mole.LP:
             pass
@@ -113,7 +132,7 @@ class WhackmoleApp(App):
     def build(self):
          game = MoleGame()
          game.start()
-         Clock.schedule_interval(game.update, 1.0)
+         Clock.schedule_interval(game.update, 1.0 / 50.0)
          #print game.hammer.center
          return game
 
